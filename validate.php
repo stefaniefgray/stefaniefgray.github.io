e142745196eea157dee1fbb5594e2ce25ae9e233
stefaniegray <stefanie.f.gray@gmail.com>
2014-12-22 03:02:05 -0500

    Fixed contact form and newsletter inputs

---
+++ validate.php
@@ -0,0 +1,19 @@
+<?php
+
+					if(isset($_POST['submit'])) {
+
+					//include validation class
+
+					//assign post data to variables
+
+					$name = trim($_POST['name']); // Storing username
+
+					$email = trim($_POST['email']); // Storing email address
+
+					$subject= trim($_POST['subject']); // storing the subject
+
+					$message= trim($_POST['message']); // storing the message
+
+					}
+
+?>
